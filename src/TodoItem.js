7361059bdddfb16ca14c300e1652d839c61c7808
mavispcchen <nehccp@gmail.com>
2019-02-13 12:37:52 -0800

    before add redux

---
+++ src/TodoItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+  render(){
+    const {item} = this.props;
+    return(
+      <div>
+        <li>
+          {item.todoName}
+          <button style = {{marginLeft: 20, marginRight: 20}}
+            onClick = {() => this.props.onDelete(item.id)}
+          >Delete
+          </button>
+        </li>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
