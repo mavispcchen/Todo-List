8af6301ccc2e76e87e9d7640ab37da7976875568
mavispcchen <nehccp@gmail.com>
2019-02-13 19:57:45 -0800

    convert todoItem to Presentational component

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TodoItem extends Component {
-  render(){
-    const {item} = this.props;
-    return(
-      <div>
-        <li>
-          {item.todo}
-          <button style = {{marginLeft: 20, marginRight: 20}}
-            onClick = {() => this.props.onDelete(item.id)}
-          >Delete
-          </button>
-        </li>
-      </div>
-    );
-  }
-}
+const TodoItem = ({ onDelete, item }) => (
+  <div>
+    <li>
+      {item.todo}
+      <button
+        style = {{marginLeft: 20, marginRight: 20}}
+        onClick = {() => onDelete(item.id)}
+      >Delete
+      </button>
+    </li>
+  </div>
+);
 
 export default TodoItem;
