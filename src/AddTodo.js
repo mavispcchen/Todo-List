47cc9d02381831f1e29361393413915c07401a82
mavispcchen <nehccp@gmail.com>
2019-02-13 22:23:59 -0800

    change addtodo to Presentational component

---
+++ src/AddTodo.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+import TodoItem from './TodoItem';
+import uuidv4 from 'uuid/v4';
+import { addTodo, removeTodo } from './actions';
+import { connect } from 'react-redux';
+
+let AddTodo = ({ dispatch, todoList }) => {
+  let input;
+  return (
+    <div className = "AddTodo">
+      <form
+        onSubmit = {(e) => {
+          e.preventDefault();
+          dispatch(addTodo(input.value, uuidv4()));
+          input.value = "";
+      }}>
+        <input
+          placeholder = "write a todo item"
+          ref={node => {
+            input = node
+          }}
+        />
+        <button
+          disabled = {input === ""}
+          type = "submit"
+        >Add
+        </button>
+      </form>
+
+      {todoList.map((item) => {
+        return (
+          <TodoItem
+            key={item.id}
+            item={item}
+            onDelete={(id) => {
+                dispatch(removeTodo(id));
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    todoList: state.todoList
+  };
+}
+export default connect(mapStateToProps)(AddTodo);
--- src/App.js
+++ src/App.js
@@ -1,68 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import TodoItem from './TodoItem';
-import uuidv4 from 'uuid/v4';
-import { connect } from 'react-redux';
-import { addTodo, removeTodo } from './actions';
+import AddTodo from './AddTodo';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      inputValue: ""
-    }
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.dispatch(addTodo(this.state.inputValue, uuidv4()));
-    this.setState({ inputValue : "" });
-  }
-
-  handleChange = (e) => {
-    this.setState({ inputValue: e.target.value });
-  }
-
-  handleDelete = (id) => {
-      this.props.dispatch(removeTodo(id));
-  }
 
   render() {
-    let todoList =[];
     return (
       <div className = "App">
-        <form onSubmit = {this.handleSubmit}>
-          <input
-            placeholder = "write a todo item"
-            onChange = {this.handleChange}
-            value = {this.state.inputValue}
-          />
-          <button
-            disabled = {this.state.inputValue === ""}
-            type = "submit"
-          >Add
-          </button>
-        </form>
-
-
-        {this.props.todoList.map((item) => {
-          return (
-            <TodoItem
-              key={item.id}
-              item={item}
-              onDelete={this.handleDelete}
-            />
-          );
-        })}
+        <AddTodo />
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    todoList: state.todoList,
-  };
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
